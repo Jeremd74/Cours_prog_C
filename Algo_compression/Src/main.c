7e17a8acb4b3329a3be52d288812a4ede84a36d0
Jeremd74 <151175123+Jeremd74@users.noreply.github.com>
2025-10-14 14:29:05 +0200

    ajout de la cration d'une feuille

--- Algo_compression/Src/main.c
+++ Algo_compression/Src/main.c
@@ -2,15 +2,18 @@
 #include <stm32f446xx.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 #include "gpio.h"
 #include "usart.h"
 #include "timer.h"
 #include "util.h"
 
-#define DEBUG_UART
+	#define DEBUG_UART_OCCURENCE
+#define DEBUG_UART_CREA_FEUILLE
 
 #define TAILLE_MAX_COMPRESS 500
+#define NB_MAX_CARACTERE 256
 
 struct noeud{
 	uint8_t c; 						//Caractere initial
@@ -18,7 +21,7 @@ struct noeud{
 	uint32_t code;					//Code binaire dans l'arbre
 	uint32_t tailleCode;			//Nombre de bits dans le code
 	struct noeud *gauche, *droite;	//Liens vers les noeuds suivants
-}
+};
 
 //Texte non compresse
 uint8_t texte[] = "aaaabbbccd";
@@ -27,7 +30,7 @@ uint8_t texte[] = "aaaabbbccd";
 uint8_t texteCompress[TAILLE_MAX_COMPRESS];
 
 //Tableau du nombre d'occurrence de chaque caractere
-uint32_t tabCaractere[256] = {0};
+uint32_t tabCaractere[NB_MAX_CARACTERE] = {0};
 
 //Nombre de caractere total dans le texte non compresse
 uint32_t nbrCaractereTotal = 0;
@@ -36,16 +39,16 @@ uint32_t nbrCaractereTotal = 0;
 uint32_t nbrCaractereDifferent = 0;
 
 //Arbre de Huffman
-struct noeud* arbreHuffman[256];
+struct noeud* arbreHuffman[NB_MAX_CARACTERE];
 
 /**
- * @brief
+ * @brief compte le nombre total de caractere differents et compte le nombre d'occurence de chaque caractere
  *
  * @param chaine adresse du tableau du texte non compresse
  * @param tab tableau qui sert a compte le nombre d'occurence dans le texte
  * @return uint32_t  nombre de caractere dans le texte
  */
-uint32_t occurence(uint8_t* chaine, uint32_t tab[256])
+uint32_t occurence(uint8_t* chaine, uint32_t tab[NB_MAX_CARACTERE])
 {
 	uint32_t ret_nbrCaractereTotal = 0;
 	uint8_t l_ui8_while = 1;
@@ -59,8 +62,7 @@ uint32_t occurence(uint8_t* chaine, uint32_t tab[256])
 		{
 			tab[chaine[iBcl]] = tab[chaine[iBcl]] + 1;
 
-
-#ifdef DEBUG_UART
+#ifdef DEBUG_UART_OCCURENCE
 			printf("caractere = %c, occurence = %i \n", chaine[iBcl], tab[chaine[iBcl]]);
 #endif
 		}
@@ -70,11 +72,43 @@ uint32_t occurence(uint8_t* chaine, uint32_t tab[256])
 
 	ret_nbrCaractereTotal = iBcl - 1;
 
-#ifdef DEBUG_UART
+#ifdef DEBUG_UART_OCCURENCE
 			printf("caractere total = %i \n", ret_nbrCaractereTotal);
 #endif
 
 	return ret_nbrCaractereTotal;
+}
+
+/**
+ * @brief Creer une feuille et avec son caractere et son nombre d'occurence
+ * 
+ * @param arbre tableau de l'arbre de Huffman
+ * @param tab Tableau d'occurence des caractere
+ */
+void creerFeuille(struct noeud* arbre[NB_MAX_CARACTERE], uint32_t tab[NB_MAX_CARACTERE])
+{
+	uint16_t l_ui8_cptArbre = 0;
+	for(uint16_t iBcl = 0; iBcl < NB_MAX_CARACTERE; iBcl++)
+	{
+		if(tab[iBcl] != 0)
+		{
+			
+			arbre[l_ui8_cptArbre]= malloc(sizeof(arbre[l_ui8_cptArbre]));
+			free(arbre[l_ui8_cptArbre]);
+			arbre[l_ui8_cptArbre]->c = iBcl;
+			arbre[l_ui8_cptArbre]->occurence = tab[iBcl];
+			arbre[l_ui8_cptArbre]->code = 0;
+			arbre[l_ui8_cptArbre]->tailleCode = 0;
+			arbre[l_ui8_cptArbre]->gauche = NULL;
+			arbre[l_ui8_cptArbre]->droite = NULL;
+
+#ifdef DEBUG_UART_CREA_FEUILLE
+			printf("caractere = %c, occurence = %i \n", arbre[l_ui8_cptArbre]->c, arbre[l_ui8_cptArbre]->occurence);
+#endif
+
+			l_ui8_cptArbre++;
+		}
+	}
 }
 
 int main(void)
@@ -86,8 +120,10 @@ int main(void)
 	GPIO_Init();
 	USART2_Init();
 	SYSTICK_Init();
-
+	printf("\n\r\n\r\n\rStart\n\r");
 	nbrCaractereTotal = occurence(texte, tabCaractere);
+
+	creerFeuille(arbreHuffman, tabCaractere);
 
 	while(1){
 		SYSTICK_Delay(500);
